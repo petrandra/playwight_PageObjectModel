9d5298c51d1e53182d96662357699658eb5b3beb
Sandra Nnedinso <sandrannedinso95@gmail.com>
2026-07-06 00:00:36 +0100

    first commit

---
+++ pages/login.js
@@ -0,0 +1,21 @@
+
+export class LoginPage {
+  constructor(page) {
+    this.page = page;
+
+    this.usernameInput = page.getByRole('textbox', { name: 'Username' });
+    this.passwordInput = page.getByRole('textbox', { name: 'Password' });
+    this.loginButton = page.getByRole('button', { name: /Login/ });
+  }
+
+  async gotoLoginPage() {
+    await this.page.goto('https://the-internet.herokuapp.com/login');
+  }
+
+  async login(username, password) {
+    await this.usernameInput.fill(username);
+    await this.passwordInput.fill(password);
+    await this.loginButton.click();
+  }
+}
+
---
+++ tests/assertion.spec.js
@@ -0,0 +1,7 @@
+// import test,{page, expect} from '@playwright/test'
+
+// test('assertions Demo', async ({ page }) => {
+//     await page.goto("https://kitchen.applitools.com/")
+//     await page.pause()
+//     await page.locator(getByRole('heading', { name: 'The Kitchen' }))
+// })
